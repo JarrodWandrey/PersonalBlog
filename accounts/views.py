70869a498fc0fa0bf4a860440265af02cc1741f9
Jarrod Wandrey <jwandrey@mybrycer.com>
2025-12-03 15:18:33 -0600

    Adds accounts app that allows for user registration and auth, updated pages to handle login/logout/register

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from .forms import SignUpForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.shortcuts import redirect
 
 # Create your views here.
+def register_user(request):
+    form = SignUpForm()
+
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            #authenticate and login the user
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                messages.success(request, 'Registration successful.')
+                return redirect('home')
+            else:
+                messages.error(request, 'Authentication failed. Please login.')
+                return redirect('login')
+        else:
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f'{error}')
+
+    return render(request, 'register.html', {'form': form})
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have been logged in.')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have been logged out.')
+    return redirect('home')
