72d8cf26da47d14ad29208d9d74ac0a1ea19102b
Jarrod Wandrey <jwandrey@mybrycer.com>
2025-12-04 15:07:17 -0600

    Adds Comment to models and adds to admin site

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,9 @@ class Post(models.Model):
     author = models.CharField(max_length=50)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.CharField(max_length=50)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
